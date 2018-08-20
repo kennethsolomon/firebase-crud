0f7873c27ac172dbc10096942d9ba4b576261930
SynecderZ <synecders@gmail.com>
2018-08-20 16:32:46 +0800

    add function

--- public/course_manager.js
+++ public/course_manager.js
@@ -2,17 +2,32 @@
 class CourseManager {
 
     constructor(theUser) {
-        // this.user = user;
+         this.user = theUser;
 
         // let email = this.user.email;
 
         // console.log(email);
         // console.log(user);
-
-        $('#add-course').one("click", () => this.addUser());
-    }
-    addUser() {
-        alert("Add User");
         
+        $('#add-course').click(() => this.addUser(theUser));
     }
+    addUser(theUser, name, description) {
+        name = document.getElementById("course-title").value;
+        description = document.getElementById("course-description").value;
+        firebase.database().ref('users/' + theUser).set({
+            name : name,
+            description : description
+        });
+      }
+        
+        // writeUserData((userId, name, email, imageUrl) =>{
+        //     alert("Add User");
+        //     var database = firebase.database();
+        //     firebase.database().ref('users/' + userId).set({
+        //       username: name,
+        //       email: email,
+        //       profile_picture : imageUrl
+        //     });
+        //   )};
+    
 }
