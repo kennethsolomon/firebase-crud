abfec4ebecca0504a4d57a7ab9a9868da54148f7
SynecderZ <synecders@gmail.com>
2018-08-20 23:14:21 +0800

    add Functions - Working on Display

--- public/course_manager.js
+++ public/course_manager.js
@@ -2,32 +2,59 @@
 class CourseManager {
 
     constructor(theUser) {
-         this.user = theUser;
+        this.user = theUser;
 
-        // let email = this.user.email;
-
-        // console.log(email);
-        // console.log(user);
-        
         $('#add-course').click(() => this.addUser(theUser));
     }
+
     addUser(theUser, name, description) {
         name = document.getElementById("course-title").value;
         description = document.getElementById("course-description").value;
-        firebase.database().ref('users/' + theUser).set({
-            name : name,
-            description : description
+        firebase.database().ref('users/' + theUser).push({
+            name: name,
+            description: description
         });
+    }
+
+    viewUsers(theUser){
+        firebase.database().ref('users/' + theUser).once('value').then((snapshot) => {
+            let courses = snapshot.val();
+            if (courses) {
+              for (let id in courses) {
+                let course = courses[id];
+                this.insertCourseInTable(id, course);
+              }
+              componentHandler.upgradeElements(document.getElementById('course-table'));
+              this.setupDeleteHandler();
+              this.setupTitleChangeHandler();
+            }
+          });
+    }
+    getMdlTableSelector() {
+        var mdlclass = "mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect mdl-data-table__select mdl-js-ripple-effect--ignore-events is-upgraded";
+        return `<td>
+          <label class="${mdlclass}" data-upgraded=",MaterialCheckbox,MaterialRipple">
+            <input type="checkbox" class="mdl-checkbox__input">
+            <span class="mdl-checkbox__focus-helper"></span>
+            <span class="mdl-checkbox__box-outline">
+              <span class="mdl-checkbox__tick-outline"></span>
+            </span>
+            <span class="mdl-checkbox__ripple-container mdl-js-ripple-effect mdl-ripple--center" data-upgraded=",MaterialRipple">
+              <span class="mdl-ripple"></span>
+            </span>
+          </label>
+        </td>`;
       }
-        
-        // writeUserData((userId, name, email, imageUrl) =>{
-        //     alert("Add User");
-        //     var database = firebase.database();
-        //     firebase.database().ref('users/' + userId).set({
-        //       username: name,
-        //       email: email,
-        //       profile_picture : imageUrl
-        //     });
-        //   )};
+      insertCourseInTable(id, course) {
+        var list = $('#course-table-body').append(
+          `<tr id="course_${id}">
+            ${this.getMdlTableSelector()}
+            <td class="mdl-data-table__cell--non-numeric course-title ">${ course.title }</td>
+            <td class="mdl-data-table__cell--non-numeric course-description">${ course.description }</td>
+            <td><i class="material-icons course-delete-row">delete</i></td>
+          </tr>`);
+        componentHandler.upgradeElements(list.children().last());
+      }
     
+
 }
